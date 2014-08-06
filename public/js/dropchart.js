3f4632e2b4a8d24d2a9fff98251d6379f83bb3bd
Anthony Schneider <anthonyserious@gmail.com>
2014-08-06 19:44:09 -0400

    Working prototype

--- public/js/dropchart.js
+++ public/js/dropchart.js
@@ -1,43 +1,39 @@
 "use strict"
-    $('#dropArea').on('dragover', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
-    $('#dropArea').on('dragenter', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
+$('#dropArea').on('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+$('#dropArea').on('dragenter', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
 
-    $('#dropArea').on('drop', function(e){
-        if(e.originalEvent.dataTransfer){
-            if(e.originalEvent.dataTransfer.files.length) {
-                e.preventDefault();
-                e.stopPropagation();
-                var file = e.originalEvent.dataTransfer.files[0];
-                var reader = new FileReader();
+$('#dropArea').on('drop', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(e.originalEvent.dataTransfer){
+        if(e.originalEvent.dataTransfer.files.length) {
+            var file = e.originalEvent.dataTransfer.files[0];
+            var reader = new FileReader();
 
-                reader.onload = function(evt) {
-                    var inData = JSON.parse(evt.target.result);
-                    console.log(evt.target.result);
-                    console.log(inData);
-                    google.load("visualization", "1", {packages:["corechart"]});
-                    var v = [
-                      ["series1", "series2"],
-                      [123, 102],
-                      [113, 112]
-                    ];
-                    function drawChart() {
-                        var chartData = google.visualization.arrayToDataTable(v);
-                        //var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-                        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-                        chart.draw(chartData, {title:"foo"});
-                    }
-                    //google.setOnLoadCallback(drawChart);
+            reader.onload = function(evt) {
+                var inData = JSON.parse(evt.target.result);
+                //console.log(evt.target.result);
+                //console.log(inData);
+                if(google) {
+                    google.load("visualization", "1.0", {
+                            packages:["corechart"],
+                            callback: function () {
+                                var chartData = google.visualization.arrayToDataTable(inData['values']);
+                                var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                                chart.draw(chartData, inData['options']);
+                            }
+                        }
+                    );
                 }
-                reader.readAsText(file);
             }
+            reader.readAsText(file);
         }
-    });
-
-    
+    }
+});
 
