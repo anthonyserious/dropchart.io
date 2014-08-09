453abdb780391581419739d31ce18aad65cf4d98
Anthony Schneider <anthonyserious@gmail.com>
2014-08-09 12:00:12 -0400

    Code refactoring, closured dropchart.

--- public/js/dropchart.js
+++ public/js/dropchart.js
@@ -1,39 +1,91 @@
 "use strict"
-$('#dropArea').on('dragover', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-});
-$('#dropArea').on('dragenter', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-});
 
-$('#dropArea').on('drop', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    if(e.originalEvent.dataTransfer){
-        if(e.originalEvent.dataTransfer.files.length) {
-            var file = e.originalEvent.dataTransfer.files[0];
-            var reader = new FileReader();
+var dropchart = function() {
+  var dropArea;
+  var chartDiv;
 
-            reader.onload = function(evt) {
-                var inData = JSON.parse(evt.target.result);
-                //console.log(evt.target.result);
-                //console.log(inData);
-                if(google) {
-                    google.load("visualization", "1.0", {
-                            packages:["corechart"],
-                            callback: function () {
-                                var chartData = google.visualization.arrayToDataTable(inData['values']);
-                                var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
-                                chart.draw(chartData, inData['options']);
-                            }
-                        }
-                    );
-                }
-            }
-            reader.readAsText(file);
-        }
+  var colorSchemes = {
+    none: [],
+    first: ["#993350", "#055333", "#394600", "#90A437", "#277455"]
+  }
+
+  var defaultOptions = {
+    title: "A chart on dropchart.io",
+    colors: colorSchemes.first,
+    backgroundColor: {
+      stroke: '#055333',
+      strokeWidth: 1
     }
-});
+  }
+
+  var chartTypes = [
+    "AreaChart",
+    "BarChart",
+    "BubbleChart",
+    "CandlestickChart",
+    "ColumnChart",
+    "ComboChart",
+    "GeoChart",
+    "Histogram",
+    "LineChart",
+    "PieChart",
+    "ScatterChart",
+    "SteppedAreaChart"
+  ];
+
+  // build a single chart
+  function drawOne(elem, options, chartType, values) {
+    if(google) {
+      google.load("visualization", "1.0", {
+              packages:["corechart"],
+              callback: function () {
+                  var chartData = google.visualization.arrayToDataTable(values);
+                  var chart = new google.visualization[chartType](elem);
+                  chart.draw(chartData, options);
+              }
+          }
+      );
+    }
+  }
+
+  return {
+    init:function(inDropArea, inChartDiv)  {
+      dropArea = inDropArea;
+      chartDiv = inChartDiv;
+
+      dropArea.on('dragover', function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+      });
+      dropArea.on('dragenter', function(e) {
+          e.preventDefault();
+          e.stopPropagation();
+      });
+
+      dropArea.on('drop', function(e){
+          e.preventDefault();
+          e.stopPropagation();
+          if(e.originalEvent.dataTransfer){
+              if(e.originalEvent.dataTransfer.files.length) {
+                  var file = e.originalEvent.dataTransfer.files[0];
+                  var reader = new FileReader();
+
+                  reader.onload = function(evt) {
+                      var inData = JSON.parse(evt.target.result);
+                      var options = defaultOptions;
+                      if (inData['options']) {
+                          for (var k in inData['options']) {
+                            options[k] = inData['options'][k];
+                          }
+                      }
+                      drawOne(chartDiv[0], options, "SteppedAreaChart", inData['values']);
+                  }
+                  reader.readAsText(file);
+              }
+          }
+      });
+    } // init
+  } // return
+
+}();
 
