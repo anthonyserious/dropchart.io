eddab315a6e383b18c89ca223226b19125c55d89
ml242 <mattlucas@gmail.com>
2014-08-13 16:15:33 -0400

    still attempting to get the png working with pure js instead of the native google code, possibly an error

--- public/js/dropchart.js
+++ public/js/dropchart.js
@@ -11,6 +11,7 @@ $('#dropArea').on('dragenter', function(e) {
 $('#dropArea').on('drop', function(e){
     e.preventDefault();
     e.stopPropagation();
+    // $("#saveButton").show();
     if(e.originalEvent.dataTransfer){
         if(e.originalEvent.dataTransfer.files.length) {
             var file = e.originalEvent.dataTransfer.files[0];
@@ -27,53 +28,58 @@ $('#dropArea').on('drop', function(e){
                                 var chartData = google.visualization.arrayToDataTable(inData['values']);
                                 var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
                                 chart.draw(chartData, inData['options']);
+                            
+
+
+
+                                // WILL THIS WORK
+
+                                // convert svg to png
+                                var svg = document.querySelector( "svg" );
+                                var svgData = new XMLSerializer().serializeToString( svg );
+                                 
+                                var canvas = document.createElement( "canvas" );
+                                var ctx = canvas.getContext( "2d" );
+                                 
+                                var img = document.createElement( "img" );
+                                img.setAttribute( "src", "data:image/svg+xml;base64," + btoa( svgData ) );
+                                img.onload = function() {
+                                    ctx.drawImage( img, 0, 0 );        
+                                    // Now is done
+                                    // console.log( canvas.toDataURL( "image/png" ) );
+                                    // canvas.toDataURL( "image/png" );
+                                    // window.open(canvas.toDataURL( "image/png" ))
+                                };
+                                var exportLink = document.createElement('a');
+                                console.log(img);
+                                exportLink.attr('href', +"'" + img.src + "'" );
+                                exportLink.text('my_chart.png');
+                                $('.new_image_container').append(exportLink);
+
+
+
+
+
+
+
+
+
                             }
                         }
                     );
                 }
             }
             reader.readAsText(file);
-            $("#saveButton").show();
         }
     }
-});
-
+}); 
 
 $('#saveButton').hide();
 
 
-$('#saveButton').click(function(){
-    // convert svg to png
-    var svg = document.querySelector( "svg" );
-    var svgData = new XMLSerializer().serializeToString( svg );
-     
-    var canvas = document.createElement( "canvas" );
-    var ctx = canvas.getContext( "2d" );
-     
-    var img = document.createElement( "img" );
-    img.setAttribute( "src", "data:image/svg+xml;base64," + btoa( svgData ) );
-     
-    img.onload = function() {
-        ctx.drawImage( img, 0, 0 );
-        
-        // Now is done
-        console.log( canvas.toDataURL( "image/png" ) );
-    };
-
-    $("body").append(img);
 
 
 
-    // var img = $('#chart_div');
-    // html2canvas(img, {
-    //     letterRendering: true,
-    //     background: undefined,
-    //     logging: true,
-    //     allowTaint: true,
-    //     onrendered: function(canvas) {
-    //     var new_img = canvas.toDataURL("image/png");
-    //     window.open(new_img);
-    //     }
-    // });
-});
+
+
 
