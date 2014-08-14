6383397eb709381c187f64d3415c70278828244a
Anthony Schneider <anthonyserious@gmail.com>
2014-08-13 20:01:48 -0400

    1.  Multiple file drop. 2.  Switched drop area to entire page. 3.  Shadow formatting of charts.

--- public/js/dropchart.js
+++ public/js/dropchart.js
@@ -3,6 +3,7 @@
 var dropchart = function() {
   var dropArea;
   var chartParent;
+  var chartInputs = [];
 
   var colorSchemes = {
     none: [],
@@ -34,64 +35,98 @@ var dropchart = function() {
   ];
 
   // build a single chart
-  function drawOne(elem, options, chartType, values) {
+  function drawAll(elem) {
+    console.log("chartInputs: "+chartInputs);
     if(google) {
       google.load("visualization", "1.0", {
               packages:["corechart"],
               callback: function () {
-                  var chartData = google.visualization.arrayToDataTable(values);
-                  var chart = new google.visualization[chartType](elem);
+                for (var i = 0; i < chartInputs.length; i++) {
+                  elem.append(
+                    '<div class="row">'
+                    +'<div class="col-md-1">&nbsp;</div>'
+                    + '<div class="col-md-10" align="center">'
+                    +   '<div id="chartDiv'+i+'" class="chartDiv drop-shadow"></div>'
+                    + '</div>'
+                    +'<div class="col-md-1">&nbsp;</div>'
+                    +'</div>');
+                  var chartData = google.visualization.arrayToDataTable(chartInputs[i].values);
+                  var chart = new google.visualization["SteppedAreaChart"](document.getElementById("chartDiv"+i));
                   //google.visualization.events.addListener(chart, 'ready', function () {
                     //elem.innerHTML = '<img src="' + chart.getImageURI() + '">';
                     //console.log(elem.innerHTML);
                   //});
-                  chart.draw(chartData, options);
+                  chart.draw(chartData, chartInputs[i].options);
+                }
               }
           }
       );
     }
   }
 
+  // When files are dropped, process them asynchronously and store the inputs in chartInputs[]
+  function readFile(file) {
+    var reader = new FileReader();
+    var deferred = $.Deferred();
+ 
+    reader.onload = function(evt) {
+        deferred.resolve(evt.target.result);
+    };
+ 
+    reader.onerror = function() {
+        deferred.reject(this);
+    };
+
+    reader.onload = function(evt) {
+      var inData = JSON.parse(evt.target.result);
+      var options = defaultOptions;
+      if (inData['options']) {
+          for (var k in inData['options']) {
+            options[k] = inData['options'][k];
+          }
+      }
+      var obj = {options: options, values: inData['values']};
+      chartInputs.push(obj);
+      deferred.resolve(evt.target.result);
+    }
+ 
+    reader.readAsText(file);
+    console.log("returning");
+    return deferred.promise();
+  } // readFile()
+
   return {
     init:function(inDropArea, inChartParent)  {
       dropArea = inDropArea;
       chartParent = inChartParent;
 
-      dropArea.on('dragover', function(e) {
-          e.preventDefault();
-          e.stopPropagation();
+      dropArea.on('dragover', function(evt) {
+          evt.preventDefault();
+          evt.stopPropagation();
       });
-      dropArea.on('dragenter', function(e) {
-          e.preventDefault();
-          e.stopPropagation();
+      dropArea.on('dragenter', function(evt) {
+          evt.preventDefault();
+          evt.stopPropagation();
       });
 
-      dropArea.on('drop', function(e){
-          e.preventDefault();
-          e.stopPropagation();
-          if(e.originalEvent.dataTransfer){
-              for (var i = 0; i < e.originalEvent.dataTransfer.files.length; i++) {
-              //if(e.originalEvent.dataTransfer.files.length) {
-                  var file = e.originalEvent.dataTransfer.files[i];
-                  var reader = new FileReader();
-                  var chartId = "chartDiv"+i;
-
-                  chartParent.append('<div class="col-md-1">&nbsp;</div><div class="col-md-10" align="center"><div id="'+chartId+'" class="chartDiv drop-shadow"></div></div><div class="col-md-1">&nbsp;</div>');
-
-                  reader.onload = function(evt) {
-                      var inData = JSON.parse(evt.target.result);
-                      var options = defaultOptions;
-                      if (inData['options']) {
-                          for (var k in inData['options']) {
-                            options[k] = inData['options'][k];
-                          }
-                      }
-                      drawOne($('#'+chartId)[0], options, "SteppedAreaChart", inData['values']);
-                  }
-                  reader.readAsText(file);
-              }
+      dropArea.on('drop', function(evt){
+        evt.preventDefault();
+        evt.stopPropagation();
+        var promises = [];
+        if(evt.originalEvent.dataTransfer){
+          for (var i = 0; i < evt.originalEvent.dataTransfer.files.length; i++) {
+            var file = evt.originalEvent.dataTransfer.files[i];
+            promises[i] = readFile(file);
           }
-      });
+          $.when.apply($, promises).done(function(a) {
+            drawAll(chartParent);
+            console.log("length: "+chartInputs.length);
+            for (var i = 0; i < chartInputs.length; i++) {
+              console.log(chartInputs[i]);
+            }
+          });
+        }
+      }); // on('drop')
     } // init
   } // return
 
