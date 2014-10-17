a28bab447ccbfbee7068238ec3f3b1eec4904742
ml242 <mattlucas@gmail.com>
2014-10-17 14:16:17 -0400

    about to add redis

--- public/js/linkedin.js
+++ public/js/linkedin.js
@@ -1,10 +1,16 @@
+var dropUser = -1;
+
 function getMember(){
   console.log('calling the member');
   IN.API.Profile("me").result(function(result) {
+    dropUser = result.values[0].id;
     // push to db
+    // debugger;
     return result.values[0];
   });
 };
+
+
 
 $(function() {
 
